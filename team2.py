c264ccef02a57b8d61324b25162f23d47bf19bbc
Re-knee <34038@d49.org>
2018-03-01 12:13:40 -0700

    final

--- team2.py
+++ team2.py
@@ -26,7 +26,10 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if their_history == "bbc":
+        return 'b' 
+    if their_score <= 250:
+        return 'c'  
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
